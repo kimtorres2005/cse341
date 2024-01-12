9021b7e15bc6213da8cd191df1bf2b83f261380e
kimtorres2005 <kimtorres2005@gmail.com>
2024-01-11 21:31:48 -0600

    going beyond on lesson 1

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const routes = require('express').Router();
+const lesson1Controller = require('../controllers/lesson1');
+
+// List of routes would go here:
+// example: routes.get('/kimberly', (req, res) => {
+
+// Moudle function goes here:
+module.exports = routes
